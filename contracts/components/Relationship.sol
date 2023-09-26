556cd7ab0d37e6a047862f9258faf33c76d6d2ee
liangqin.fan <liangqin.fan@gmail.com>
2023-09-26 09:29:28 +0800

    add: finish Relationship

--- contracts/components/Relationship.sol
+++ contracts/components/Relationship.sol
@@ -5,28 +5,26 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 
-import "./RelationshipBase.sol";
-
 contract Relationship is ERC721Enumerable {
     using EnumerableSet for EnumerableSet.UintSet;
     using EnumerableSet for EnumerableSet.AddressSet;
 
-    uint256 public constant RootParentId = 100000000000;
+    uint256 public constant RootId = 100000000000;
     address public constant RootWallet   = address(0x000000000000000000000000000000000000dEaD);
 
     uint256 public constant MaxParentsLevel = 5;
 
-    // relations about reputationId, its parent id stored at index 0.
-    // ReputationId => [ReputatonId]
-    mapping(uint256 => EnumerableSet.UintSet) relations;
-    // bind wallet and reputationId
-    // ReputationId => [address]
+    // idRelations about relationId, its parent id stored at index 0.
+    // RelationId => [ReputatonId]
+    mapping(uint256 => EnumerableSet.UintSet) idRelations;
+    // bind wallet and relationId
+    // RelationId => [address]
     mapping(uint256 => EnumerableSet.AddressSet) idToWallets;
     // reverse bind from wallet to reputstionId
-    // address => ReputationId
+    // address => RelationId
     mapping(address => uint256) walletsToId;
     // which wallet used to receive benefit
-    // reputationId => address
+    // relationId => address
     mapping(uint256 => address) beneficialWallets;
 
     error ParentExist(uint256 existParent, uint256 targetParent);
@@ -39,7 +37,7 @@ contract Relationship is ERC721Enumerable {
     );
 
     event BeneficialSet(
-        uint256 indexed reputationId,
+        uint256 indexed relationId,
         address indexed oldBeneficial,
         address indexed newBeneficial
     );
@@ -49,13 +47,13 @@ contract Relationship is ERC721Enumerable {
         uint256 sonId,
         address sonWallet
     ) internal {
-        if (relations[sonId].length() > 0)
-            revert ParentExist(relations[sonId].at(0), parentId);
-        relations[sonId].add(parentId);
+        if (idRelations[sonId].length() > 0)
+            revert ParentExist(idRelations[sonId].at(0), parentId);
+        idRelations[sonId].add(parentId);
 
-        if (relations[parentId].contains(sonId))
+        if (idRelations[parentId].contains(sonId))
             revert SonExist(parentId, sonId);
-        relations[parentId].add(sonId);
+        idRelations[parentId].add(sonId);
 
         if (!idToWallets[sonId].contains(sonWallet)) idToWallets[sonId].add(sonWallet);
 
@@ -64,16 +62,16 @@ contract Relationship is ERC721Enumerable {
         emit RelationBound(parentId, sonId, sonWallet);
     }
 
-    constructor() ERC721("Bee Smart Reputation", "BSP") {
-        _mint(RootWallet, RootParentId); // mint RootParentId to a black hold address
+    constructor() ERC721("Bee Smart Relationship", "BSR") {
+        _mint(RootWallet, RootId); // mint RootId to a black hold address
     }
 
     function nextId() public view returns(uint256) {
-        return RootParentId + totalSupply();
+        return RootId + totalSupply();
     }
 
     // bind the parent and son relationship.
-    // if soneId is 0, then we should mint a Reputation NFT for msg.sender at first.
+    // if soneId is 0, then we should mint a Relationship NFT for msg.sender at first.
     function bind(uint256 parendId, uint256 sonId) external {
         require(ownerOf(parendId) != address(0), "parent id not exist");
         if (sonId == 0) {
@@ -90,30 +88,61 @@ contract Relationship is ERC721Enumerable {
         _bind(parendId, sonId, msg.sender);
     }
 
-    function setBeneficialWallet(address beneficial) external {
-        require(beneficial != address(0), "beneficial is 0");
+    function setBeneficial(address wallet) external {
+        require(wallet != address(0), "beneficial is 0");
         uint256 id = walletsToId[msg.sender];
         address oldBeneficial = beneficialWallets[id];
         require(oldBeneficial == msg.sender, "only old beneficial allowed");
 
-        beneficialWallets[id] = beneficial;
+        beneficialWallets[id] = wallet;
 
-        emit BeneficialSet(id, oldBeneficial, beneficial);
+        emit BeneficialSet(id, oldBeneficial, wallet);
     }
 
-    function getBeneficialWallets(address sonWallet) public view returns(address[] memory) {
-        address[] memory wallets = new address[](MaxParentsLevel);
+    function getBeneficial(address wallet) external view returns(address) {
+        uint256 id = walletsToId[wallet];
+        return beneficialWallets[id];
+    }
 
-        uint256 reputationId = walletsToId[sonWallet];
-        if (reputationId == 0 || reputationId == RootParentId) return wallets;
+    function getRelationId(address wallet) external view returns(uint256) {
+        return walletsToId[wallet];
+    }
+
+    function getWallets(uint256 relationId) external view returns(address[] memory) {
+        // CAUTION: would bond too many wallets to enumerate, if happen
+        return idToWallets[relationId].values();
+    }
+
+    //
+    function getParentRelationId(address wallet) external view returns(uint256[] memory) {
+        uint256[] memory ids = new uint256[](MaxParentsLevel);
+        uint256 relationId = walletsToId[wallet];
+        if (relationId == 0 || relationId == RootId) return ids;
 
         uint256 parendId;
         for (uint i = 0; i < MaxParentsLevel; ++i) {
-            parendId = relations[reputationId].at(0);
-            if (parendId == RootParentId) break;
+            parendId = idRelations[relationId].at(0);
+            if (parendId == RootId) break;
+            ids[i] = parendId;
+
+            relationId = parendId;
+        }
+        return ids;
+    }
+
+    function getParentBeneficials(address sonWallet) public view returns(address[] memory) {
+        address[] memory wallets = new address[](MaxParentsLevel);
+
+        uint256 relationId = walletsToId[sonWallet];
+        if (relationId == 0 || relationId == RootId) return wallets;
+
+        uint256 parendId;
+        for (uint i = 0; i < MaxParentsLevel; ++i) {
+            parendId = idRelations[relationId].at(0);
+            if (parendId == RootId) break;
             wallets[i] = beneficialWallets[parendId];
 
-            reputationId = parendId;
+            relationId = parendId;
         }
         return wallets;
     }
